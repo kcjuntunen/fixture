1d5c7201e631f1fadb6285e9f1e5f0c714d12d67
K. C. Juntunen <kcjuntunen@frontier.com>
2016-03-17 11:23:28 +0000

    Changed tab size

--- thingspeak/ip.py
+++ thingspeak/ip.py
@@ -2,9 +2,11 @@ import socket
 import fcntl
 import struct
 import smtplib
+import time
 import json
+import array
 
-def send_email(subj, ip_addr):
+def send_email(subj, msg):
     try:
         sender = config_data["sender"]
         receivers = config_data["recipients"]
@@ -20,9 +22,9 @@ def send_email(subj, ip_addr):
                 message += ", "
 
         message += "Subject: "
-            
+
         message += subj + "\n\n"
-        message += "Pi started. IP: " + ip_addr + "\n"
+        message += msg + "\n"
         smtpo = smtplib.SMTP(config_data["smtp_server"])
         smtpo.sendmail(sender, receivers, message)
         print "Sent email"
@@ -30,14 +32,14 @@ def send_email(subj, ip_addr):
         print "Failure sending email. %s" % e
 
 def get_ip_address(ifname):
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  return socket.inet_ntoa(fcntl.ioctl(
-    s.fileno(),
-    0x8915,  #SIOCGIFADDR
-    struct.pack('256s', ifname[:15]))[20:24])
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  #SIOCGIFADDR
+        struct.pack('256s', ifname[:15]))[20:24])
 
 def all_interfaces():
-    max_possible = 128  # arbitrary. raise if needed.
+    max_possible = 128
     bytes = max_possible * 32
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     names = array.array('B', '\0' * bytes)
@@ -50,16 +52,16 @@ def all_interfaces():
     return [namestr[i:i+32].split('\0', 1)[0] for i in range(0, outbytes, 32)]
 
 def broadcast_ip():
-  global config_data
-  with open('/etc/thingspeak.json') as data_file:
-    config_data = json.load(data_file)
+    global config_data
+    with open('/etc/thingspeak.json') as data_file:
+        config_data = json.load(data_file)
 
-  send_email(config_data["unit"] + " booted.", get_ip_address('wlan0'))
-  
+    while 'wlan0' not in all_interfaces():
+        time.sleep(15)
+
+    send_email("Unit: " + config_data["unit"] + " booted.",
+               "IP: " + get_ip_address('wlan0'))
+
 
 if __name__ == "__main__":
-  global config_data
-  with open('/etc/thingspeak.json') as data_file:
-    config_data = json.load(data_file)
-
-  send_email(config_data["unit"] + " booted.", get_ip_address('wlan0'))
+    broadcast_ip()
